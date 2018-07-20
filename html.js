0300e2a5ad3a603e3d084e9494bf254b79817bd0
oktot <38215780+oktot@users.noreply.github.com>
2018-07-20 13:13:40 +0700

    Update html.js

--- html.js
+++ html.js
@@ -70,7 +70,7 @@ function buildGoogleAnalyticsSetup() {
 function buildGoogleAdSense() {
   const js = `
   (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: 'ca-pub-',
+    google_ad_client: 'ca-pub-3917963024669400',
     enable_page_level_ads: true
   });
   `;
